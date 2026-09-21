4178025728bd390a9a809a29d543f4900f9e51e8
nuratto <nuratto05@gmail.com>
2026-09-21 17:07:19 -0500

    add telemetry table, api

--- sat-ground-station/Controllers/TelemetryController.cs
+++ sat-ground-station/Controllers/TelemetryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sat_ground_station.Models;
 using sat_ground_station.Repository;
 
 /// <summary>
@@ -20,22 +22,44 @@ public class TelemetryController : ControllerBase
         _telemetryRepository = telemetryRepository;
     }
 
-    [HttpPost]
-    public IActionResult AddTelemetry()
+    [HttpGet]
+    public async Task<ActionResult<List<Telemetry>>> Get100MostRecentTelemetries()
     {
+        List<Telemetry> telemetries =  await _telemetryRepository.Telemetries.OrderByDescending(t => t.Timestamp).Take(100).ToListAsync();
 
-        
+        if (telemetries == null || telemetries.Count == 0)
+        {
+            return NotFound();
+        }
 
-        return Ok("Hello from satellites");
+        return telemetries;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<List<Telemetry>>> Get100SpecificTelemetries(int id)
+    {
+        if (id == null) return NotFound();
+
+        List<Telemetry> telemetries =  await _telemetryRepository.Telemetries.OrderByDescending(t => t.SatelliteId).Take(100).ToListAsync();
+
+        if(telemetries == null || telemetries.Count == 0)
+        {
+            return NotFound();
+        }
+
+        return telemetries;
     }
 
     [HttpPost]
-    public IActionResult AddTelemetry()
+    public async Task<ActionResult> AddTelemetry([FromBody] Telemetry tel)
     {
 
-        
+        if (tel == null) return BadRequest("Invalid Request");
 
-        return Ok("Hello from satellites");
+        await _telemetryRepository.Telemetries.AddAsync(tel);
+        await _telemetryRepository.SaveChangesAsync();
+
+        return Ok(tel);
     }
 
 }
--- sat-ground-station/Models/Telemetry.cs
+++ sat-ground-station/Models/Telemetry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace sat_ground_station.Models;
 
@@ -10,27 +11,35 @@ public class Telemetry
 	[Column("id")]
 	public int Id {  get; set; }
 
-	[Column("satalliteId")]
+    [Required]
+    [Column("satalliteId")]
 	public int SatelliteId {  get; set; }
 
+	[Required]
     [Column("timestamp")]
     public DateTime Timestamp { get; set; }
 
+    [Required]
     [Column("longitude")]
     public double Longitude { get; set; }
 
+    [Required]
     [Column("latitude")]
     public double Latitude { get; set; }
 
+    [Required]
     [Column("altitude")]
     public int Altitude { get; set; }
 
+    [Required]
     [Column("velocity")]
     public int Velocity { get; set; }
 
+    [Required]
     [Column("temperature")]
     public int Temperature { get; set; }
 
+    [Required]
     [Column("batteryLevel")]
     public int BatteryLevel { get; set; }
 
